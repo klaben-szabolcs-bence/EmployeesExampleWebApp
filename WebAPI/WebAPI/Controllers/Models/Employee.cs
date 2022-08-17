bfdc4a2beae94984c1c4fa02dcdad953710f297f
Tudi20 <tudorkae@gmail.com>
2022-08-17 19:22:53 +0200

    Add records according to database schema

---
+++ WebAPI/WebAPI/Controllers/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Controllers.Models
+{
+    public record Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
