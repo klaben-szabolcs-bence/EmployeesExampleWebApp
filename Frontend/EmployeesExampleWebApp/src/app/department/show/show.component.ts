a3532736d7456dfaeb5f0811448d8b39b44cd900
Tudi20 <tudorkae@gmail.com>
2022-08-19 17:24:47 +0200

    Add department listing

--- Frontend/EmployeesExampleWebApp/src/app/department/show/show.component.ts
+++ Frontend/EmployeesExampleWebApp/src/app/department/show/show.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/models/department';
+import { WebAPIService } from 'src/app/web-api.service';
 
 @Component({
-  selector: 'app-show',
+  selector: 'app-show-department',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
 export class ShowDepartmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: WebAPIService) { }
+
+  departmentList: Department[] = [];
 
   ngOnInit(): void {
+    this.refreshDepartmentList();
+  }
+
+  refreshDepartmentList() {
+    this.service.getDepartments().subscribe(
+      result => {
+        this.departmentList = result;
+      }
+    );
+  }
+
+  onEdit(department: Department) {
+    console.log('Edit ' + department.DepartmentID);
+  }
+
+  onDelete(department: Department) {
+    console.log('Delete' + department.DepartmentID);
   }
 
 }
