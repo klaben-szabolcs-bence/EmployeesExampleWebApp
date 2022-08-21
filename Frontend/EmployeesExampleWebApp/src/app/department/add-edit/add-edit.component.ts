c07cf7bbfa5160c65e9a2f25b228e76318ebb42c
Tudi20 <tudorkae@gmail.com>
2022-08-21 14:49:45 +0200

    Add functionality to the button on Department

--- Frontend/EmployeesExampleWebApp/src/app/department/add-edit/add-edit.component.ts
+++ Frontend/EmployeesExampleWebApp/src/app/department/add-edit/add-edit.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Department } from 'src/app/models/department';
+import { WebAPIService } from 'src/app/web-api.service';
+import { ShowDepartmentComponent } from '../show/show.component';
+
 
 @Component({
-  selector: 'app-add-edit',
+  selector: 'app-add-edit-department',
   templateUrl: './add-edit.component.html',
   styleUrls: ['./add-edit.component.css']
 })
 export class AddEditDepartmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: WebAPIService) { }
+
+  @Input()
+  ShowDepComp: ShowDepartmentComponent | undefined;
+  DepartmentID: number | undefined;
+  DepartmentName: string | undefined;
+  @Input()
+  modal: any;
 
   ngOnInit(): void {
+    this.DepartmentID = this.ShowDepComp?.department.DepartmentID;
+    this.DepartmentName = this.ShowDepComp?.department.DepartmentName;
   }
 
+  addDepartment() {
+    if (this.DepartmentID == undefined || this.DepartmentName == undefined) {
+      console.log("DepartmentID or DepartmentName is undefined");
+      return;
+    }
+    var newDepartment = { DepartmentID: this.DepartmentID, DepartmentName: this.DepartmentName };
+    this.api.addDepartment(newDepartment).subscribe(
+      result => {
+        this.ShowDepComp?.closeModal(this.modal, "Added");
+        this.ShowDepComp?.showSuccess(result.Message);
+      }
+    );
+  }
+
+  updateDepartment() {
+    if (this.DepartmentID == undefined || this.DepartmentName == undefined) {
+      console.log("DepartmentID or DepartmentName is undefined");
+      return;
+    }
+    var newDepartment = { DepartmentID: this.DepartmentID, DepartmentName: this.DepartmentName };
+    this.api.updateDepartment(newDepartment).subscribe(
+      result => {
+        this.ShowDepComp?.closeModal(this.modal, "Updated");
+        this.ShowDepComp?.showSuccess(result.Message);
+      }
+    );
+  }
 }
