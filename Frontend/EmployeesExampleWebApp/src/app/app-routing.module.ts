6e068e5e6f702f577728743b0f2a8a1a0d188587
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 15:57:48 +0200

    Add routing

--- Frontend/EmployeesExampleWebApp/src/app/app-routing.module.ts
+++ Frontend/EmployeesExampleWebApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DepartmentComponent } from './department/department.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+const routes: Routes = [
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'department', component: DepartmentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
