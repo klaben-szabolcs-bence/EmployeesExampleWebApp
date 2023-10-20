58fd1e0140540ca5240510e5bff880cdf2c07714
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 15:57:45 +0200

    Add records according to database schema

---
+++ WebAPI/WebAPI/Controllers/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Controllers.Models
+{
+    public record Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
